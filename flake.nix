b09995b483c8ebf4e398959dca583dc0f9ddabd3
Sleroq <hireme@sleroq.link>
2025-06-05 22:53:34 +0300

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -7,33 +7,127 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        
-        reactor = pkgs.buildGoModule rec {
-          pname = "reactor";
-          version = "0.1.0";
+    let
+      # Define the package builder function
+      buildReactor = pkgs: pkgs.buildGoModule rec {
+        pname = "reactor";
+        version = "0.1.0";
 
-          src = ./.;
+        src = ./.;
 
-          vendorHash = "sha256-oKoSCT2lpykWoU6dkD36XryieZb7VAdEs3S7ZrJqPbQ=";
+        vendorHash = "sha256-oKoSCT2lpykWoU6dkD36XryieZb7VAdEs3S7ZrJqPbQ=";
 
-          subPackages = [ "src" ];
+        subPackages = [ "src" ];
 
-          postInstall = ''
-            mv $out/bin/src $out/bin/reactor
-          '';
+        postInstall = ''
+          mv $out/bin/src $out/bin/reactor
+        '';
 
-          meta = with pkgs.lib; {
-            description = "A Telegram Bot for monitoring and forwarding messages";
-            homepage = "https://github.com/sleroq/reactor";
-            license = licenses.gpl3Only;
-            maintainers = [ ];
-            platforms = platforms.unix;
+        meta = with pkgs.lib; {
+          description = "A Telegram Bot for monitoring and forwarding messages";
+          homepage = "https://github.com/sleroq/reactor";
+          license = licenses.gpl3Only;
+          maintainers = [ ];
+          platforms = platforms.unix;
+        };
+      };
+    in
+    {
+      # NixOS module at top level
+      nixosModules.reactor = { config, lib, pkgs, ... }:
+        with lib;
+        let
+          cfg = config.services.reactor;
+        in {
+          options.services.reactor = {
+            enable = mkEnableOption "Reactor Telegram Bot";
+
+            package = mkOption {
+              type = types.package;
+              default = buildReactor pkgs;
+              description = "The reactor package to use";
+            };
+
+            user = mkOption {
+              type = types.str;
+              default = "reactor";
+              description = "User to run reactor as";
+            };
+
+            group = mkOption {
+              type = types.str;
+              default = "reactor";
+              description = "Group to run reactor as";
+            };
+
+            dataDir = mkOption {
+              type = types.str;
+              default = "/var/lib/reactor";
+              description = "Directory to store reactor data";
+            };
+
+            environmentFile = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = "File containing environment variables";
+            };
+
+            settings = mkOption {
+              type = types.attrs;
+              default = { };
+              description = "Environment variables for reactor";
+            };
+          };
+
+          config = mkIf cfg.enable {
+            users.users.${cfg.user} = {
+              isSystemUser = true;
+              group = cfg.group;
+              home = cfg.dataDir;
+              createHome = true;
+            };
+
+            users.groups.${cfg.group} = { };
+
+            systemd.services.reactor = {
+              description = "Reactor Telegram Bot";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+
+              serviceConfig = {
+                Type = "simple";
+                User = cfg.user;
+                Group = cfg.group;
+                WorkingDirectory = cfg.dataDir;
+                ExecStart = "${cfg.package}/bin/reactor";
+                Restart = "always";
+                RestartSec = "5s";
+
+                NoNewPrivileges = true;
+                PrivateTmp = true;
+                ProtectSystem = "strict";
+                ProtectHome = true;
+                ReadWritePaths = [ cfg.dataDir ];
+                ProtectKernelTunables = true;
+                ProtectKernelModules = true;
+                ProtectControlGroups = true;
+              };
+
+              environment = cfg.settings // {
+                REACTOR_SESSION_DIR = cfg.dataDir;
+              };
+            } // lib.optionalAttrs (cfg.environmentFile != null) {
+              serviceConfig.EnvironmentFile = cfg.environmentFile;
+            };
           };
         };
 
+      # Default nixos module for easy import
+      nixosModules.default = self.nixosModules.reactor;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        reactor = buildReactor pkgs;
       in
       {
         packages.default = reactor;
@@ -66,94 +160,6 @@
 
           CGO_ENABLED = "1";
         };
-
-        nixosModules.reactor = { config, lib, pkgs, ... }:
-          with lib;
-          let
-            cfg = config.services.reactor;
-          in {
-            options.services.reactor = {
-              enable = mkEnableOption "Reactor Telegram Bot";
-
-              package = mkOption {
-                type = types.package;
-                default = reactor;
-                description = "The reactor package to use";
-              };
-
-              user = mkOption {
-                type = types.str;
-                default = "reactor";
-                description = "User to run reactor as";
-              };
-
-              group = mkOption {
-                type = types.str;
-                default = "reactor";
-                description = "Group to run reactor as";
-              };
-
-              dataDir = mkOption {
-                type = types.str;
-                default = "/var/lib/reactor";
-                description = "Directory to store reactor data";
-              };
-
-              environmentFile = mkOption {
-                type = types.nullOr types.str;
-                default = null;
-                description = "File containing environment variables";
-              };
-
-              settings = mkOption {
-                type = types.attrs;
-                default = { };
-                description = "Environment variables for reactor";
-              };
-            };
-
-            config = mkIf cfg.enable {
-              users.users.${cfg.user} = {
-                isSystemUser = true;
-                group = cfg.group;
-                home = cfg.dataDir;
-                createHome = true;
-              };
-
-              users.groups.${cfg.group} = { };
-
-              systemd.services.reactor = {
-                description = "Reactor Telegram Bot";
-                wantedBy = [ "multi-user.target" ];
-                after = [ "network.target" ];
-
-                serviceConfig = {
-                  Type = "simple";
-                  User = cfg.user;
-                  Group = cfg.group;
-                  WorkingDirectory = cfg.dataDir;
-                  ExecStart = "${cfg.package}/bin/reactor";
-                  Restart = "always";
-                  RestartSec = "5s";
-
-                  NoNewPrivileges = true;
-                  PrivateTmp = true;
-                  ProtectSystem = "strict";
-                  ProtectHome = true;
-                  ReadWritePaths = [ cfg.dataDir ];
-                  ProtectKernelTunables = true;
-                  ProtectKernelModules = true;
-                  ProtectControlGroups = true;
-                };
-
-                environment = cfg.settings // {
-                  REACTOR_SESSION_DIR = cfg.dataDir;
-                };
-              } // lib.optionalAttrs (cfg.environmentFile != null) {
-                serviceConfig.EnvironmentFile = cfg.environmentFile;
-              };
-            };
-          };
 
         apps.default = {
           type = "app";
